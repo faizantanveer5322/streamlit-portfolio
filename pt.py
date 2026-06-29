1869c00bd69185e713f9ebebd658b561db8181e1
faizantanveer5322 <faizantanveer5322@gmail.com>
2026-06-29 06:16:13 -0700

    pt.py

--- pt.py
+++ pt.py
@@ -18,7 +18,7 @@ st.markdown('<meta name="google-site-verification" content="TBLIhmsG0kd1b0" />',
 
 
 # Custom CSS for stunning design
-st.markdown("""
+
     <style>
 # Custom CSS for stunning design
 st.markdown("""
