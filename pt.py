b857e1baa5dad5c5172dd8f19c5e78db2dc7e939
faizantanveer5322 <faizantanveer5322@gmail.com>
2026-06-30 05:55:36 -0700

    pt.py

--- pt.py
+++ pt.py
@@ -12,7 +12,7 @@ import time
 # Page configuration
 st.set_page_config(
     page_title="Faizan Tanveer ",
-    page_icon="👨‍💻",
+    page_icon="💻",
     layout="wide",
     initial_sidebar_state="expanded"
 )
